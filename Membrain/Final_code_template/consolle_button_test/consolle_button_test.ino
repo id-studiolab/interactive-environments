92be89061ce81ad5e87710fdc71d04eb0f5c0973
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-01-23 13:55:28 +0100

    final commit

--- Membrain/Final_code_template/consolle_button_test/consolle_button_test.ino
+++ Membrain/Final_code_template/consolle_button_test/consolle_button_test.ino
@@ -3,21 +3,20 @@
 #include <avr/power.h>
 #endif
 
-#include <SoftwareSerial.h>
-
 #define rxPin 10
 #define txPin 11
 
-int buttonPins [4] = { 4, 3, 2, 19 };
+int buttonPins [4] = { 3, 4, 2, 19 };
 int ledNumbers [4] = { 6, 6, 10, 12 };
-int ledPins [4] = { 9, 8, 7, 6 };
+int ledPins [4] = { 8, 9, 7, 6 };
+
+int lastButtonStates[4];
 
 Adafruit_NeoPixel pixels_FS = Adafruit_NeoPixel(ledNumbers[0], ledPins [0], NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels_forest = Adafruit_NeoPixel(ledNumbers[1], ledPins [1], NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels_climate = Adafruit_NeoPixel(ledNumbers[2], ledPins [2], NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels_playback = Adafruit_NeoPixel(ledNumbers[3], ledPins [3], NEO_GRB + NEO_KHZ800);
 
-SoftwareSerial lolin (rxPin, txPin);
 
 unsigned long lastActivityCheck = 0;
 unsigned long lastSent = 0;
@@ -31,7 +30,6 @@ void setup() {
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   Serial.begin(9600);
-  lolin.begin(9600);
   //lolin.listen();
 
   pixels_FS.begin(); // This initializes the NeoPixel library.
@@ -39,29 +37,30 @@ void setup() {
   pixels_climate.begin();
   pixels_playback.begin();
 
-  subscribeToChannel("/membrane/mode");
-
 }
 
 void loop() {
 
   readButtons();
   updateLeds();
-  checkIncomingmsg();
 }
 
 
 void readButtons() {
   for (int i = 0; i < 4; i++) {
-    if (digitalRead(buttonPins[i]) == HIGH && activeMode != i) {
+    int buttonState=digitalRead(buttonPins[i]);
+    int lastButtonState=lastButtonStates[i];
+    
+    if (buttonState && !lastButtonState) {
       activeMode = i;
-      Serial.print("mode ");
-      Serial.print( i);
-      Serial.println(" active");
+//      Serial.print("mode ");
+//      Serial.print( activeMode);
+//      Serial.println(" active");
 
-      sendMsg("/membrane/mode", String(activeMode));
-
+      Serial.println(activeMode);
     }
+    lastButtonStates[i]=buttonState;
+
   }
 }
 
@@ -109,42 +108,42 @@ void setledStripColor(int ledStripIndex, int r, int g, int b) {
 }
 
 
-//here 3 functions to send/receive msg from/to the broker
-void subscribeToChannel(String topic) {
-  lolin.print("subscribe-");
-  lolin.print("topic");
-  lolin.print('\n');
-}
-
-void sendMsg(String topic, String msg) {
-  lolin.print(topic);
-  lolin.print("-");
-  lolin.print(msg);
-  lolin.print('\n');
-}
-
-void checkIncomingmsg() {
-
-  if (lolin.available() > 0) {
-
-    Serial.println("gotdata");
-    String command = lolin.readStringUntil('\n');
-    Serial.println(command);
-
-//    int separatorIndex = command.indexOf('-');
+////here 3 functions to send/receive msg from/to the broker
+//void subscribeToChannel(String topic) {
+//  lolin.print("subscribe-");
+//  lolin.print("topic");
+//  lolin.print('\n');
+//}
 //
-//    if (separatorIndex > 1) {
-//      String arg1 = command.substring(0, separatorIndex);
-//      String arg2 = command.substring(separatorIndex + 1, command.length());
+//void sendMsg(String topic, String msg) {
+//  lolin.print(topic);
+//  lolin.print("-");
+//  lolin.print(msg);
+//  lolin.print('\n');
+//}
+
+//void checkIncomingmsg() {
 //
-//      Serial.println ("--------");
-//      Serial.println ("received msg:");
-//      Serial.println (arg1);
-//      Serial.println (arg2);
-//      Serial.println ("--------");
-//    }
-  }
-}
+//  if (lolin.available() > 0) {
+//
+//    Serial.println("gotdata");
+//    String command = lolin.readStringUntil('\n');
+//    Serial.println(command);
+//
+////    int separatorIndex = command.indexOf('-');
+////
+////    if (separatorIndex > 1) {
+////      String arg1 = command.substring(0, separatorIndex);
+////      String arg2 = command.substring(separatorIndex + 1, command.length());
+////
+////      Serial.println ("--------");
+////      Serial.println ("received msg:");
+////      Serial.println (arg1);
+////      Serial.println (arg2);
+////      Serial.println ("--------");
+////    }
+//  }
+//}
 
 
 
