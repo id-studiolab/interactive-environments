e301ce3ac6c5c2d2ae23188460fa97c97a8859b2
Thijmen Langendam <thijmenlangendam@gmail.com>
2019-01-30 17:46:35 +0100

    Add commenting to NFC Module

--- cozone/nfcModule/nfcModule/nfcModule.ino
+++ cozone/nfcModule/nfcModule/nfcModule.ino
@@ -2,19 +2,23 @@
 #include <MFRC522.h>
 #include <Adafruit_NeoPixel.h>
 
+// Initialize NFC LED strip (2 rows of 6 pixels)
 #define PIN            7
 #define NUMPIXELS      12
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+// Define 2 pins for the MFRC522 NFC board
 #define RST_PIN         9           // Configurable, see typical pin layout above
 #define SS_PIN          10          // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, 3);   // Create MFRC522 instance
 
+// Create data structure for the "checked-in" keycard UIDs
 String uids[6];
 
 int prevTime;
 
+// Function that returns the index of the to be found UID
 int hasUID(String toFind) {
   for (int i = 0; i < 6; i++) {
     if (uids[i] == toFind) {
@@ -24,6 +28,7 @@ int hasUID(String toFind) {
   return -1;
 }
 
+// Function that returns the next available index for the UID data structure
 int nextIndex() {
   for (int i = 0; i < 6; i++) {
     if (uids[i] == "") {
@@ -33,6 +38,7 @@ int nextIndex() {
   return -1;
 }
 
+// Function that is used to initialize the UID data structure
 void fillUIDS() {
   for (int i = 0; i < 6; i++) {
     uids[i] = "";
@@ -48,6 +54,7 @@ void setup() {
   prevTime = millis();
 }
 
+// Function that fleshes all NFC LEDs red once if this CoZone is full
 void flashLEDS() {
   for (int i = 0; i < 6; i++) {
     pixels.setPixelColor(i, 50, 0, 0);
@@ -82,6 +89,8 @@ void flashLEDS() {
 
 void loop() {
   int amount = 0;
+  
+  // Turn on the lights if someone is checked in
   for (int i = 0; i < 6; i++) {
     if (uids[i] != "") {
       amount++;
@@ -94,22 +103,26 @@ void loop() {
   }
   pixels.show();
 
+  // Send data to the attached WeMoS board every other 5 seconds
   if (millis() - prevTime > 5000) {
     Serial.println(String(amount));
     prevTime = millis();
   }
 
+  // Edge cases for malfunctioning RFID-Scanner
   if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-
   if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
 
+  // Read the UID if an RFID-Tag is presented to the module
   String readUID = String(mfrc522.uid.uidByte[0]) + String(mfrc522.uid.uidByte[1]) + String(mfrc522.uid.uidByte[2]) + String(mfrc522.uid.uidByte[3]);
+  // If the card isn't recognize, initialize it, otherwise log the person out.
   if (hasUID(readUID) == -1) {
     int next = nextIndex();
+    // If there's a spot available, save the tag UID, otherwise flash
     if (next != -1) {
       uids[next] = readUID;
     } else {
